16528380d1b1bb6da371ec04945f4df71ce47444
cbdppk <dkjunior250@gmail.com>
2023-02-27 04:19:08 -0800

    hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
-/**
- * main- stderr
- *
- * Return: 1
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
  */
 
+#include <unistd.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
 }
