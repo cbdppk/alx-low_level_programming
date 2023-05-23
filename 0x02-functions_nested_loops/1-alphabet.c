ba6dc30b355b36668ba3ef0a3ba781c6bbf8cf5c
cbdppk <dkjunior250@gmail.com>
2023-05-23 11:54:14 -0700

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - for printing alphabets
+ *
+ * Return: returns nothing
+ */
+
+void print_alphabet(void)
+{
+	char a;
+	
+	a = 'a';
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
