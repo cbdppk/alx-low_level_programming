3a137069f2de0f948e9e84f61fa7a5fb65652ebc
cbdppk <dkjunior250@gmail.com>
2023-05-23 16:01:03 -0700

    c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - for printing thesign of numbers
+ *
+ * @n: arguement for the function
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
