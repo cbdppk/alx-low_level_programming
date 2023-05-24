0d2ebe0b750a5780dcec3db8f77d3cd7bd1420ae
cbdppk <dkjunior250@gmail.com>
2023-05-23 20:08:02 -0700

    h

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - for getting the absolute of a numb
+ * @a: arguement for the function
+ *
+ * Return: always return 0;
+ */
+
+int _abs(int a)
+{
+	abs(a);
+	
+	return (0);
+}
