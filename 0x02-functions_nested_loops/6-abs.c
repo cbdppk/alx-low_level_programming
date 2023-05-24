98b11988bbc150be6541b04a51fa1ea74f84d02f
cbdppk <dkjunior250@gmail.com>
2023-05-23 21:07:26 -0700

    last digit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -11,5 +11,5 @@
 
 int _abs(int a)
 {
-	return abs(a);
+	return (abs(a));
 }
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - for printing the last digit
+ * @a: only arguement
+ *
+ * Return: return the last value of a number
+ */
+
+int print_last_digit(int a)
+{
+	return (a % 10);
+}
