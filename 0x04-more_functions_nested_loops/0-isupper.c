a1e88a920e103d1a570b7afe6341c34bf677bc44
cbdppk <dkjunior250@gmail.com>
2023-05-25 13:41:19 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - for checking for upper cases
+ * @c: the arguement
+ *
+ * Return: returns 1 if true and 0 if false
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
