4af957fa686eb617511299834e8025a397604f77
cbdppk <dkjunior250@gmail.com>
2023-05-29 16:14:09 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - for getting the string length
+ *
+ * @s : first arguement
+ *
+ * Return: returns len;
+ */
+
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
