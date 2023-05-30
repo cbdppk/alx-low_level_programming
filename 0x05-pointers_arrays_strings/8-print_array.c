1cf66014af2de6447e82601726852f8540b6cda3
cbdppk <dkjunior250@gmail.com>
2023-05-29 17:50:47 -0700

    arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints the elements in array
+ * @a : first arguement
+ * @n : second arguement
+ */
+
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0; b < n; b++)
+	{
+		if (b != (n - 1))
+		{
+			printf("%d, ", a[b]);
+		}
+		else
+		{
+			printf("%d, ", a[b]);
+		}
+	}
+	_putchar('\n');
+}
