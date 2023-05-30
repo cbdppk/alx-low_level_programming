d1dac3a5e93592e41a087966feccedaaf9a45c15
cbdppk <dkjunior250@gmail.com>
2023-05-30 15:51:59 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strcpy - for copying a string to the dest
+ * @dest: the first arguement
+ * @src: second arguement
+ *
+ * Return: returns a char;
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
