c0c4e58325aa397f1f9e8df0d31fa3a5abe3af08
cbdppk <dkjunior250@gmail.com>
2023-06-01 10:10:05 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - for concanating two strings
+ * @dest : first arguement
+ * @src : second arguement
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
