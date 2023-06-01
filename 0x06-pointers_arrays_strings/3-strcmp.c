77c79485a1d847e7f2b62039cbfdeeee834ced89
cbdppk <dkjunior250@gmail.com>
2023-06-01 14:54:39 -0700

    strncmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcmp - for comparing two strings
+ * @s1 : first arguement
+ * @s2 : second arguement
+ * Return: returns 0 or <0 or >0
+ */
+
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
