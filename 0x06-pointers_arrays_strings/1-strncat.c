c95f50e5065876b02b6931c08a27e2c63700b3fe
cbdppk <dkjunior250@gmail.com>
2023-06-01 10:30:23 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * char *_strncat(char *dest, char *src, int n) -
+ * @dest : first arguement 
+ * @ src : second arguement 
+ * @n : third arguement
+ * Return: returns e the concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+
+}
