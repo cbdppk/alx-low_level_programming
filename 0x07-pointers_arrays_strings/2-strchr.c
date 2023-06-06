032fd39865d4c56d55ad27002dd1f39bae1b387b
cbdppk <dkjunior250@gmail.com>
2023-06-05 19:27:57 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - returns a pointer on occurence
+ * @s: first aguement
+ * @c: second arguement
+ *
+ * Return: returns char
+ */
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
