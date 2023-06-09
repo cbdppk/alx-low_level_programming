654d419ffa23330ebd042d39e22bfacd3742f1aa
cbdppk <dkjunior250@gmail.com>
2023-06-08 22:39:51 -0700

    r

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - for printing using recursion
+ * @s: first arguement
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		puts(*s);
+		_puts_recursion(s + 1);
+	}
+}
