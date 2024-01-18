54600b7c4fa1becaa84ea6b1ad891a024cb8eacd
cbdppk <122842339+cbdppk@users.noreply.github.com>
2024-01-18 06:24:57 -0600

    pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - for reversing a string
+ * @s : a string
+ * return : returns 0;
+ */
+
+void rev_string(char *s)
+{
+	int len;
+	int n;
+	int a;
+	int p;
+	int k;
+	int l;
+
+	len = strlen(s);
+	a = len - 1;
+	char c[a];
+
+	for (n = a, p = 0; n >= 0, p <= a; n--, p++)
+	{
+		c[p] = s[n];
+
+	}
+	for (k = 0; k <= a; k++)
+	{
+		s[k] = c[k];
+	}
+}
