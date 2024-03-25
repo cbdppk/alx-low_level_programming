62056adb91a313f3eef831da7aec169d8fa77977
cbdppk <dkjunior250@gmail.com>
2024-03-25 11:52:13 -0400

    h

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,32 +1,29 @@
-#include "lists.h"
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include "lists.h"
 
 /**
- * print_list - for accessing and printing data in lists
- * @h: argument
- * Return: returns 0
+ * print_list - for printing the list
+ * @h: the only argument
+ *
+ * Return: returns count
  */
-
 
 size_t print_list(const list_t *h)
 {
 	int count;
 
-	count = 0;
-	while (h)
+	if (h->str == NULL)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-		printf("[%d] %s\n", h->len, h->str);
-		}
+		printf("[0] (nil)\n");
+	}
+	while (h->str != NULL)
+	{
 		count++;
+		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}
+
 	return (count);
 }
