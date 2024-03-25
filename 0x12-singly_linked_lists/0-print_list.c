ff8453547b7a9926ea9eea0c375222c0168a307c
cbdppk <dkjunior250@gmail.com>
2024-03-25 05:50:52 -0400

    singly lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - for accessing and printing data in lists
+ * @h: argument
+ * Return: returns 0
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count;
+	list_t *ptr;
+
+	ptr = NULL;
+	count = 0;
+
+	if (head == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+
+	while (ptr != NULL)
+	{
+		count++;
+		printf("[%d] %s\n", ptr->len, ptr->str);
+		ptr = ptr->next;
+	}
+
+	return (count);
+}
