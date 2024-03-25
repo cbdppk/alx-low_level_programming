67264399cffe6e167ca78a2add3fde5d6e6b1ae3
cbdppk <dkjunior250@gmail.com>
2024-03-25 12:01:40 -0400

    singly

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,16 +14,18 @@ size_t print_list(const list_t *h)
 {
 	int count;
 
-	if (h->str == NULL)
+	while (h)
 	{
-		printf("[0] (nil)\n");
-	}
-	while (h->str != NULL)
-	{
-		count++;
+		if (h->str = NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
 		printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
 		h = h->next;
 	}
-
 	return (count);
 }
