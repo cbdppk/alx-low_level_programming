c7e8486919ac508eb95ee98ddf9e9bb650aa448a
danielk872 <juniordanielk872@gmail.com>
2024-04-18 06:22:04 -0500

    data

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,6 +21,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 
 	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
