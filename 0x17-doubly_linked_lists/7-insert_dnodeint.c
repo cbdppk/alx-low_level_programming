eba174447a7c6ed75de46bb78b6910188ab9ee34
danielk872 <juniordanielk872@gmail.com>
2024-04-23 05:30:13 -0500

    data

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -46,9 +46,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		num++;
 	}
 	temp = ptr->next;
-	ptr->next = new_node;
-	temp->prev = new_node;
 	new_node->next = temp;
 	new_node->prev = ptr;
+	if (ptr->next != NULL)
+		temp->prev = new_node;
+	ptr->next = new_node;
 	return (new_node);
 }
