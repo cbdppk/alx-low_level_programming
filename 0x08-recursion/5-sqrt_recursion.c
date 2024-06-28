c8a46eec1872af8cc426ddf50a5887d46a075a82
danielk872 <juniordanielk872@gmail.com>
2024-06-28 17:16:56 -0500

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,6 +3,30 @@
 #include <math.h>
 
 /**
+ * find_number - for finding the number
+ * @n: the number to find it's square root
+ * @guess: the loop or the iteration for finding the natural square root
+ * Return: returns int
+ */
+
+int find_number(int n, int guess)
+{
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+	else if (guess * guess > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (find_number(n, guess + 1));
+	}
+}
+
+
+/**
  * _sqrt_recursion - for finding the square root of a number
  * @n : the number
  * Return: returns int
@@ -10,13 +34,16 @@
 
 int _sqrt_recursion(int n)
 {
-	double result;
-
 	if (n < 0)
 	{
 		return (-1);
 	}
-	result = sqrt(n);
-
-	return (result);
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (find_number(n, 1));
+	}
 }
