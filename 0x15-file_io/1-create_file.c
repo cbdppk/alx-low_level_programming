0a8ef3dc79115b87887be736eb9a6c6eb79a8960
danielk872 <juniordanielk872@gmail.com>
2024-09-11 12:41:13 -0500

    file i/o

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_file - for creation and writing a file
+ * @filename: the name of the filename
+ * @text_content: the text to be written
+ * Return: -1 or 1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	return (1);
+}
