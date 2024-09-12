41b6ff09b230ab2eba459099cdc9a6cda7a26a80
danielk872 <juniordanielk872@gmail.com>
2024-09-11 21:27:31 -0500

    file input and outputs

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,115 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * copy_to - for copying data from file_from to file_to
+ * @file_from: the file containing the data to be copied
+ * @file_to: the file to recieve the data
+ * Return: returns 0
+ */
+
+int copy_to(char *file_from, char *file_to)
+{
+	int fd1;
+	int fd2;
+	ssize_t bytes_read;
+	ssize_t bytes_written;
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+	fd1 = open(file_from, O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		free(buffer);
+		exit(98);
+	}
+
+	fd2 = open(file_to, O_RDWR | O_CREAT | O_TRUNC, 0664);
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		if ((close(fd1) == -1))
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd1);
+			exit(100);
+		}
+		free(buffer);
+		exit(99);
+	}
+
+	while ((bytes_read = read(fd1, buffer, 1024)) > 0)
+	{
+		bytes_written = write(fd2, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			free(buffer);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			if (close(fd2) == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd2);
+				exit(100);
+			}
+			if (close(fd1) == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd1);
+				exit(100);
+			}
+			exit(99);
+		}
+
+	}
+
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		if (close(fd2) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd2);
+			exit(100);
+		}
+		if (close(fd1) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd1);
+			exit(100);
+		}
+		return (98);
+	}
+
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd2);
+		free(buffer);
+		exit(100);
+	}
+	if (close(fd1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd1);
+		free(buffer);
+		exit(100);
+	}
+
+	free(buffer);
+	return (0);
+}
+
+/**
+ * main - for copying data from one file to the other
+ * @argc: the arguement count
+ * @argv: the arguement vectors
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	copy_to(argv[1], argv[2]);
+
+	return (0);
+}
