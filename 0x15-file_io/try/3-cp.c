83d62062696c90d3793700389915f2d2240f4813
danielk872 <juniordanielk872@gmail.com>
2024-09-16 15:14:23 -0500

    he

--- 0x15-file_io/try/3-cp.c
+++ 0x15-file_io/try/3-cp.c
@@ -23,7 +23,7 @@ void copy_to(char *file_from, char *file_to)
 		exit(98);
 	}
 
-	fd2 = open(file_to, O_RDWR | O_CREAT | O_TRUNC, 0664);
+	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
@@ -31,7 +31,7 @@ void copy_to(char *file_from, char *file_to)
 		exit(99);
 	}
 
-	while ((bytes_read = read(fd1, buffer, 1024)) > 0)
+	while ((bytes_read = read(fd1, buffer, sizeof(buffer))) > 0)
 	{
 		bytes_written = write(fd2, buffer, bytes_read);
 		if (bytes_written == -1)
